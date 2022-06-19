2172008aa232baf8330bd496a41b0bb63f453a73
Whitney Harden <wtharden320@gmail.com>
2022-06-19 12:49:05 -0400

    working from quick start tutorial. Now moving on to V2

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ I am following a tutorial to learn more about the Spotify Web Playback SDK
 * SDK ==> Software Development Kit;
 * Oxford Definition: a set of tools for third-party developers to use in producing applications using a particular framework or platform.
 
-* I used dotenv and created a .env file to run locally to conceal my Access Token
+* I used dotenv and created a .env file to run locally to conceal my Access Token - this didn't work because I couldn't use "require" or "import". I had to insert my Access code directly
+
+* I was able to get the Quick Start version of the app started. Now my next step is to create V2 where I recreate the app with the Authorization flow so I don't have to manually update with a new code every hour.
--- script.js
+++ script.js
@@ -1,36 +1,83 @@
-require('dotenv').config();
-
 window.onSpotifyWebPlaybackSDKReady = () => {
-          const token = process.env.MY_ACCESS_CODE;
-        
-          const player = new Spotify.Player({
-            name: 'Web Playback SDK Quick Start Player',
-            getOAuthToken: cb => { cb(token); },
-            volume: 0.5
-          });
+  // const play = ({
+  //   spotify_uri,
+  //   playerInstance: {
+  //     _options: {
+  //       getOAuthToken
+  //     }
+  //   }
+  // }) => {
+  //   getOAuthToken(access_token => {
+  //     fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+  //       method: 'PUT',
+  //       body: JSON.stringify({ uris: [spotify_uri] }),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Authorization': `Bearer ${access_token}`
+  //       },
+  //     });
+  //   });
+  // };
 
-          // Ready
-          player.addListener('ready', ({ device_id }) => {
-            console.log('Ready with Device ID', device_id);
-          });
+  // play({
+  //   playerInstance: new Spotify.Player({ name: "..." }),
+  //   spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
+  // });
 
-          // Not Ready
-          player.addListener('not_ready', ({ device_id }) => {
-            console.log('Device ID has gone offline', device_id);
-          });
+  // V1
+  const token = 'BQAAOLg7fbT4AAGL7d3UF2Fi4T6nFgWSnP7p3hXyR3bcLijFozjcRV_xItl44pS-y1nWAnqgQfDBxQAP4SPX-1zNVOz2N515CAPzQ5JfQfbahceWKGsJsTQ0UmRxKf046PDtAujh4YKN1IVHUkLEmtDKpNicDimTiFTnOUhFimOUqYLsoIB5wrTkb9snUfBFs1K8';
 
-          player.addListener('initialization_error', ({ message }) => { 
-              console.error(message);
-          });
+// V2
+  // const player = new Spotify.Player({
+  //   name: 'Carly Rae Jepsen Player',
+  //   getOAuthToken: callback => {
+  //     // Run code to get a fresh access token
+  //     //what is this code?
 
-          player.addListener('authentication_error', ({ message }) => {
-              console.error(message);
-          });
+  //     callback('access token here');
+  //   },
+  //   volume: 0.5
+  // });
 
-          player.addListener('account_error', ({ message }) => {
-              console.error(message);
-          });
+  //V1
+  const player = new Spotify.Player({
+    name: 'Web Playback SDK Quick Start Player',
+    getOAuthToken: cb => { cb(token); },
+    volume: 0.5
+  });
 
-           player.connect();
+  // Ready
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+  });
 
-        }
+  // Not Ready
+  player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
+  });
+
+  player.addListener('initialization_error', ({ message }) => { 
+      console.error(message);
+  });
+
+  player.addListener('authentication_error', ({ message }) => {
+      console.error(message);
+  });
+
+  player.addListener('account_error', ({ message }) => {
+      console.error(message);
+  });
+
+  player.connect().then(success => {
+    if (success) {
+      console.log('The Web Playback SDK successfully connected to Spotify!');
+    }
+  })
+
+  document.getElementById('togglePlay').onclick = function() {
+    player.togglePlay();
+  };
+
+  
+
+}
